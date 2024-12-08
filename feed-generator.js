967ac8e3254b65ffd07772431d9b46f1d02b4ffe
veganhashtag <veganhashtag@gmail.com>
2024-12-08 05:54:33 +0000

    Add files via upload

---
+++ feed-generator.js
@@ -0,0 +1,16 @@
+// Vegan Humor Feed Generator
+const HASHTAGS = ['#veganhumor', '#veganhumour', '#veganmemes', '#veganlols', '#veganhashtag'];
+const KEYWORDS = ['vegan', 'veganism', 'veganize', 'plant-based', 'veganuary', 'tofu', 'seitan'];
+const HUMOR_WORDS = ['funny', 'lol', 'joke', 'meme', '😂', '🤣', 'humor'];
+
+module.exports = {
+  generateFeed: (posts) => {
+    return posts.filter(post => {
+      const text = post.text.toLowerCase();
+      const hasHashtag = HASHTAGS.some(tag => text.includes(tag));
+      const hasKeyword = KEYWORDS.some(keyword => text.includes(keyword));
+      const hasHumor = HUMOR_WORDS.some(word => text.includes(word));
+      return hasHashtag || (hasKeyword && hasHumor);
+    });
+  }
+};
